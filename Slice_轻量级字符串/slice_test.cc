2b96d1bf5a1b1c6b2b7595ad109b706c0054d8a9
huhupei <291921096@qq.com>
2025-10-05 12:51:05 +0800

    修改 slice 非法访问示例

--- Slice_轻量级字符串/slice_test.cc
+++ Slice_轻量级字符串/slice_test.cc
@@ -1,29 +1,43 @@
 #include <string>
 #include <iostream>
-#include "slice.h"
 
-using std::string;
-using std::cout;
-using std::endl;
+#include "leveldb/slice.h"
+
 using leveldb::Slice;
+using namespace std;
 
-//可以实现对原始str的任意切片操作
-int main(){
+int main() {
     const char* str = "hello world";
     //切片操作 str[0:5) == "hello"
     Slice s1(str, 5);
     //切片操作 str[6:n) == "world"
     Slice s2(str+6, 5);
-    // s3 引用了局部字符串，这是非法操作
+    std::cout << "s1: " << s1.data() << std::endl;
+    std::cout << "s2: " << s2.data() << std::endl;
+    
     Slice s3;
-    if(1){
-        const char* str2 = "abc";
-        Slice s4(str2);
-        s3 = s4;
+    Slice s4;
+    {
+        // str2 指向字符串字面量，存储在静态数据区
+        const char* str1 = "abc";
+        // s3 指向的是静态数据，是安全的 
+        Slice s3(str1);
+        // string 内部字符串数据存储在堆内存上，在对象销毁时，内存会被释放
+        std::string str2("def");
+        // s4 指向堆内存
+        s4 = Slice(str2);
+        std::cout << "s4: " << s4.data() << std::endl;
     }
-
-    cout << s1.ToString() << endl;
-    cout << s2.ToString() << endl;
-    cout << s3.ToString() << endl;
+    // str2 已销毁，内存释放被系统回收，str3 会重新使用回收后的内存 
+    string str3("hhh");
+    // s4 指向已释放的内存（该内存已被 str3 重使用），属于非法访问，操作不安全   
+    std::cout << "s4: "<< s4.data() << std::endl;
     return 0;
 }
+
+/* Output:
+s1: hello world
+s2: world
+s4: def
+s4: hhh
+*/
